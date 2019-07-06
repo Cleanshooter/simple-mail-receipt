b6fec9d80aeb35c406dc19880d41df66b31d2560
Joe Motacek <cleanshooter@gmail.com>
2019-07-06 15:35:27 -0500

    logging adjustments

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.NODE_ENV === 'production' ? 25 : 7025;
+console.log(`NODE ENV: ${process.env.NODE_ENV}`);
+const port = 25; // process.env.NODE_ENV === 'production' ? 25 : 7025;
 
 const server = new SMTPServer({
+  disabledCommands: ['STARTTLS', 'AUTH'],
+  logger: true,
   onData(stream, session, callback) {
     stream.pipe(process.stdout); // print message to console
     stream.on('end', callback);
   },
 });
 
-server.listen(port);
+server.listen(port, () => console.log(`Mail server listening on port ${port}`));
 
 app.get('/', (req, res) => {
   res.send('HEY!');
